5f6d7d8d39d37aa8bd414ce203d35039ef644726
AntonZatsepilin <zatsepilin90.00@bk.ru>
2024-12-04 15:07:55 +0300

    Add User model with fields for ID, Username, Password, and Email

---
+++ pkg/models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	ID       int    `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+	Password string `json:"password" db:"password"`
+	Email    string `json:"email" db:"email"`
+}
